3726ad3582e4fb1899986a59b3d26fefd5e6a54f
Mohammed Elgaily <mbajily@gmail.com>
2023-09-10 09:19:52 -0700

    Write a program that prints[master e25b842] Write a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,15 @@
  * in lowercase, excluding the letters 'q' and 'e',
  * followed by a newline character. It uses the putchar
  * function to print each valid letter
- * of the alphabet sequentially, and skips printing 
+ * of the alphabet sequentially, and skips printing
  * the letters 'q' and 'e' using an if statement.
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'q' && letter != 'e')
