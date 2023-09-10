2b7056ba6373859a29c8f76a7961cabc5449def0
Mohammed Elgaily <mbajily@gmail.com>
2023-09-10 07:52:03 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,19 @@
 #include <unistd.h>
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * It uses the write function from the unistd.h header to write the message to the standard error.
+ *
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 	write(2, MESSAGE, sizeof(MESSAGE) - 1);
-	return 1;
+	return (1);
 }
