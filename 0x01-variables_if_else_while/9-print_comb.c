43e3b8fc012f54636a3e7d9183281ae0251083e0
Mohammed Elgaily <mbajily@gmail.com>
2023-09-11 00:28:30 -0700

    Write a program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible combinations
+ * of single-digit numbers 
+ * in ascending order, separated by a comma and a space.
+ * It achieves this by using a loop and the `putchar` function.
+ * The loop iterates from '0' to '9', and within each iteration,
+ * it prints the current digit using `putchar`. After printing each digit,
+ * a comma and a space are printed,
+ * except for the last digit. Finally, a newline character is printed using
+ * another `putchar` call to create a new lin
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
