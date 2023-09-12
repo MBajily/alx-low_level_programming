356df556d870eee9ea4b9414424ec4ff2a52aa15
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 16:05:30 -0700

    You have access to the full output of the Checker - Use it with reason!

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,42 +18,42 @@ int print_last_digit(int number)
 	l = number % 10;
 	if (l == 0)
 	{
-		return ("00");
+		return (00);
 	}
 	else if (l == 1)
 	{
-		return ("11");
+		return (11);
 	}
 	else if (l == 2)
 	{
-		return ("22");
+		return (22);
 	}
 	else if (l == 3)
 	{
-		return ("33");
+		return (33);
 	}
 	else if (l == 4)
 	{
-		return ("4");
+		return (4);
 	}
 	else if (l == 5)
 	{
-		return ("55");
+		return (55);
 	}
 	else if (l == 6)
 	{
-		return ("66");
+		return (66);
 	}
 	else if (l == 7)
 	{
-		return ("77");
+		return (77);
 	}
 	else if (l == 8)
 	{
-		return ("88");
+		return (88);
 	}
 	else if (l == 9)
 	{
-		return ("99");         
+		return (99);         
 	}
 }
