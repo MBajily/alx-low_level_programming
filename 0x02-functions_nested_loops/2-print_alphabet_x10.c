f97ca7863f2f5894a10109bc946b8538493b4ff9
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 04:26:40 -0700

    Compile program

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
+	char letter;
 	int i;
 
 	for (i = 0; i < 10 ; i++)
 	{
+		letter = 'a';
 		while (letter <= 'z')
 		{
 			putchar(letter);
