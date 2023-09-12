9da377d2eefbe256c7b146e51549174ed9ab49b6
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 01:14:24 -0700

    add a description to 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,11 @@
+/**
+ * Checks whether a character is a lowercase letter.
+ *
+ * @param c The ASCII value of the character
+ * to be checked.
+ * @return 1 if the character is a
+ * lowercase letter, 0 otherwise.
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
