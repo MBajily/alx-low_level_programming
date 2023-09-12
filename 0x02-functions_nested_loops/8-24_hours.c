aad03867af43bc384be770cf49640af8749d95cd
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 14:14:12 -0700

    Compile program

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,17 +2,17 @@
 
 
 /**
- * Prints every minute of the day of Jack Bauer,
+ * jack_bauer - Prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
- * The function iterates through each hour
- * (0 to 23) and each minute (0 to 59)
+ * @hour: The function iterates through each hour
+ * @minute: (0 to 23) and each minute (0 to 59)
  * using nested loops.
  *
- * It prints the hour and minute in the format
+ * Return: It prints the hour and minute in the format
  * "hh:mm" for  * each minute of the day,
  * with leading zeros if necessary.
  */
-void jack_bauer()
+void jack_bauer(void)
 {
 	for (int hour = 0; hour < 24; hour++)
 	{
