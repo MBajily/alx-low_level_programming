0ad25850dabebb68c930ab09523c1696750ba022
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 14:29:53 -0700

    Compile program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,11 @@
 
 
 /**
- * Function to print all natural numbers
- * from n to 98, separated by a comma
+ * print_to_98 - Function to print all natural numbers
+ * from
+ * @n: to 98, separated by a comma
  * and space, followed by a new line.
+ * Return: 0
  */
 void print_to_98(int n)
 {
