40b15a0d346108e8a82e70d1e11dc4f0f706d5a8
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 15:29:22 -0700

    Compile program

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,6 +15,6 @@ int print_last_digit(int number)
 	int last_digit;
 
 	last_digit = number % 10;
-	last_digit = "%d%d", last_digit, last_digit;
+	last_digit = last_digit * 10 + last_digit;
 	return (last_digit);
 }
