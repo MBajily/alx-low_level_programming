263283bb5c58652331c012e3effd8ae855dc731c
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 13:46:44 -0700

    Compile program

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * Prints the sign of a number and
+ * print_sign - Prints the sign of a number and
  * returns a corresponding value.
  *
- * @param n The number to check the sign of.
- * @return 1 and prints '+' if n is greater
+ * @n: The number to check the sign of.
+ * Return: 1 and prints '+' if n is greater
  * than zero 0 and prints '0' if n is zero.
  * -1 and prints '-' if n is less than zero.
  */
