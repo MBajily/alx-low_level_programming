c0ba98a6b58bc50dc40f94f510126e7ca1496d5d
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 14:30:38 -0700

    Compile program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,11 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -23,7 +25,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
