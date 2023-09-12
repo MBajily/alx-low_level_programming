b03bd187a5baf8353605942927b6e7e552157af7
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 15:00:43 -0700

    Compile program

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+/**
+ * print_fibonacci_numbers - Prints the first n
+ * Fibonacci numbers, separated by a comma and
+ * a space.
+ *
+ * @n: The number of Fibonacci numbers to print.
+ *
+ * Return: None.
+ */
+void print_fibonacci_numbers(int n)
+{
+	unsigned long long int a, b, c;
+	int i;
+
+	a = 1;
+	b = 1;
+	printf("%llu, %llu", a, b);
+	for (i = 3; i <= n; i++)
+	{
+		c = a + b;
+		printf(", %llu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+}
+
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the
+ * command-line arguments.
+ *
+ * Return: 0 if the program executed successfully,
+ * 1 otherwise.
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+	if (argc != 2)
+	{
+		printf("Usage: %s <n>\n", argv[0]);
+		return (1);
+	}
+	n = atoi(argv[1]);
+	print_fibonacci_numbers(n);
+	return (0);
+}
