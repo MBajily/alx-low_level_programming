8db792afaca179d138ec494c8483cf173951144e
Mohammed Elgaily <mbajily@gmail.com>
2023-09-12 13:37:42 -0700

    Compile program

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 /**
- * Checks whether a character is a lowercase letter.
- *
+ * _islower - Checks whether a character is a lowercase letter.
+ * c
  * @param c The ASCII value of the character
  * to be checked.
  * @return 1 if the character is a
  * lowercase letter, 0 otherwise.
+ *
+ * Return 1, 0
  */
 int _islower(int c)
 {
