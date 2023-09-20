5b443b57f5d387ebbbf15e834f15891a352f78e9
Mohammed Elgaily <mbajily@gmail.com>
2023-09-20 11:36:00 -0700

    Write a function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,8 +10,8 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char;
-	char;
+	char a;
+	char b;
 
 	a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
