e2e3c08e9a5bfc6bf3ea0eb19b1e5987e138e15e
Mohammed Elgaily <mbajily@gmail.com>
2023-10-22 07:06:21 -0700

    quiz #0.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,12 @@
 #define LISTS_H
 
 
-typedef struct list_s {  char *str;  unsigned int len;  struct list_s *next; } list_t;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 
 #endif
