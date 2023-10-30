2645abb11283a9f8d47cdd20aaf9f35dee3a5965
Mohammed Elgaily <mbajily@gmail.com>
2023-10-29 18:20:03 -0700

    Fix compiling errors.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,22 +7,25 @@
  * *_strchr - a function that adds a new node
  * at the beginning of a list_t list.
  *
- * @c: char
- * @s: string
+ * @str: new string
+ * @head: pointer
  *
  * Return: the address of the new element,
  * or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int sub;
-
-	while (1)
-	{
-		sub = *s++;
-		if (sub == c)
-			return (s - 1);
-		if (sub == 0)
-			return (NULL);
-	}
+	list_t *newNode;
+	unsigned int length = 0;
+	
+	while (str[length])
+		length++;
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL);
+	newNode->str = strdup(str);
+	newNode->length = length;
+	newNode->next = (*head);
+	(*head) = newNode;
+	return (*head);
 }
