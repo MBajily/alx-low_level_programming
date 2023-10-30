9948d04045d8c96a2e5a8e7f49008550a407413b
Mohammed Elgaily <mbajily@gmail.com>
2023-10-29 18:27:47 -0700

    Fix compiling errors.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,12 +17,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 	unsigned int length = 0;
-	
+
 	while (str[length])
 	{
 		length++;
 	}
 	newNode = malloc(sizeof(list_t));
+
 	if (!newNode)
 	{
 		return (NULL);
