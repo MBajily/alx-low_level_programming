5fafa42f66d7eb2850df7de7d628e88e1260b1f6
Mohammed Elgaily <mbajily@gmail.com>
2023-10-29 18:14:31 -0700

    Write a function that adds a new node at the beginning of a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * *_strchr - a function that adds a new node
+ * at the beginning of a list_t list.
+ *
+ * @c: char
+ * @s: string
+ *
+ * Return: the address of the new element,
+ * or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int sub;
+
+	while (1)
+	{
+		sub = *s++;
+		if (sub == c)
+			return (s - 1);
+		if (sub == 0)
+			return (NULL);
+	}
+}
