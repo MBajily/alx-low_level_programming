2ceef381369252d2effb09df727097959add0c33
Mohammed Elgaily <mbajily@gmail.com>
2023-10-30 18:51:42 -0700

    Add a libarary.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 
 /**
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,17 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /* MAIN_H */
