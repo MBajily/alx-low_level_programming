a344560b9da1e9c8627082aa278226216b6e5024
Mohammed Elgaily <mbajily@gmail.com>
2023-11-03 15:54:50 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	filee = open(filename, O_CREATE | O_RDWR | O_TRUNC | 0600);
+	filee = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
 	if (filee < 0)
 	{
 		return (-1);
