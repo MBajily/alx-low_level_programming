d3e054f88b409d90dd3d57076eda95f9f20366c9
Mohammed Elgaily <mbajily@gmail.com>
2023-11-03 15:52:59 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * create_file - a function that creates a file.
+ *
+ * @text_content: is a NULL terminated string to
+ * write to the file
+ * @filename: is the name of the file to create
+ *
+ * Return: 1 on success, -1 on failure (file can
+ * not be created, file can not be written,
+ * write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int x, y, filee;
+
+	y = 0;
+	if (!filename)
+	{
+		return (-1);
+	}
+	filee = open(filename, O_CREATE | O_RDWR | O_TRUNC | 0600);
+	if (filee < 0)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[y])
+		{
+			y++;
+		}
+		x = write(filee, text_content, y);
+		if (x != y)
+		{
+			return (-1);
+		}
+	}
+	close(filee);
+	return (1);
+}
