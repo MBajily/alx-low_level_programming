84fa6928e38b8b1c24eb0758a9e55a7b86d372ae
Mohammed Elgaily <mbajily@gmail.com>
2023-12-23 14:38:57 -0800

    Fix compiling errors.

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *nd, *tp;
 	unsigned long int j;
 
-	for (j = 0; j < ht->size; i++)
+	for (j = 0; j < ht->size; j++)
 	{
 		if (ht->array[j] != NULL)
 		{
