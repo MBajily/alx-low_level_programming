46798008b86c8fc8e91b1d2a7bc2fc74ebecbabf
Mohammed Elgaily <mbajily@gmail.com>
2023-12-23 14:27:39 -0800

    Fix styling errors.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - a function that creates a hash table.
+ * hash_table_create - a function that creates
+ * a hash table.
  *
  * @size: The size of the array.
  *
@@ -28,6 +29,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		ht->array[i] = NULL;
 	}
-
 	return (ht);
 }
