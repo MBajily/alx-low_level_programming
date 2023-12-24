79c36f6149a2f55255ac0b18dc3990a94fb45f00
Mohammed Elgaily <mbajily@gmail.com>
2023-12-24 13:46:59 -0800

    Add comments.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - a function that creates
+ * *hash_table_create - a function that creates
  * a hash table.
  *
  * @size: The size of the array.
